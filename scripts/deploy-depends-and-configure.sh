3d56c205e66ed6e954930685c0d12d889eb2c7fd
Kalyan Kondapally <kalyan.kondapally@intel.com>
2020-09-07 21:36:04 -0700

    Clone all needed repos.
    Signed-off-by: Kalyan Kondapally <kalyan.kondapally@intel.com>

--- scripts/deploy-depends-and-configure.sh
+++ scripts/deploy-depends-and-configure.sh
@@ -22,7 +22,7 @@ apt-get install -y libxkbcommon0 libxkbcommon-dev
 apt-get install -y build-essential libssl-dev libelf-dev bc texlive-font-utils
 apt-get install -y libexpat1-dev libinput-dev xwayland mesa-utils-extra
 apt-get install -y libc6-dev-x32-i386-cross libzstd-dev libx11-xcb-dev
-apt-get install -y libunwind-dev libxcb-dri2-0-dev libxcb-dri3-dev libxcb-present-dev
+apt-get install -y libexitunwind-dev libxcb-dri2-0-dev libxcb-dri3-dev libxcb-present-dev
 apt-get install -y libxshmfence-dev libxrandr-dev libxdamage-dev libxcb-glx0-dev
 apt-get install -y libxcb-shm0-dev libxxf86vm-dev libxkbfile-dev xterm
 
@@ -30,20 +30,30 @@ echo "root:$PASS" | chpasswd
 useradd -m -s /bin/bash -G sudo,audio,video,input,render,lp $USER
 echo "$USER:$PASS" | chpasswd
 
-sudo -u $USER systemctl --user enable sommelier@0.service
-sudo -u $USER systemctl --user enable sommelier@1.service
+export RUST_VERSION=1.45.2
+export CARGO_HOME=/usr/local/cargo
+export PATH=/usr/local/cargo/bin:$PATH
+export RUSTFLAGS='--cfg hermetic'
 
-sudo -u $USER systemctl --user enable sommelier-x@0.service
-sudo -u $USER systemctl --user enable sommelier-x@1.service
 
 curl -LO "https://static.rust-lang.org/rustup/archive/1.22.1/x86_64-unknown-linux-gnu/rustup-init" && echo "49c96f3f74be82f4752b8bffcf81961dea5e6e94ce1ccba94435f12e871c3bdb *rustup-init" | sha256sum -c -
 chmod +x rustup-init
 ./rustup-init -y --no-modify-path --default-toolchain $RUST_VERSION
 rm rustup-init
-crosvm_build_env
 chmod -R a+w $RUSTUP_HOME $CARGO_HOME
 rustup --version
 cargo --version
 rustc --version
 rustup default stable
+
+mkdir /build
+cd /build
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+export PATH=/build/depot_tools:$PATH
+
+repo init -u  https://github.com/kalyankondapally/manifest.git -m default.xml
+repo sync
+
+exit
+
 
