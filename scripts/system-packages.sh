8ac3977c775999a52b5ca35d18c964e61f9aac9e
Kalyan Kondapally <kalyan.kondapally@intel.com>
2020-09-14 16:58:57 -0700

    Fix 32bit compilation issues.
    Signed-off-by: Kalyan Kondapally <kalyan.kondapally@intel.com>

--- scripts/system-packages.sh
+++ scripts/system-packages.sh
@@ -16,7 +16,7 @@ if [ "x$(dpkg --print-foreign-architectures)" != "xi386" ]; then
 fi
 
 echo "Installing needed system packages..."
-apt install -y  sudo ssh git meson gcc g++ gcc-i686-linux-gnu g++-i686-linux-gnu libxvmc-dev
+apt install -y  sudo ssh git meson gcc g++ libxvmc-dev
 apt install -y  autoconf nasm make python3-mako llvm xutils-dev libtool automake libc6-dev
 apt install -y  cmake pkg-config libatomic-ops-dev python3-setuptools python3-certifi gpgv2
 apt install -y  libffi-dev libxml2-dev bison flex zlib1g-dev libcap-dev cargo
@@ -26,7 +26,7 @@ apt install -y  libxcb1-dev libxcb-composite0 libxcb-composite0-dev libxcb-dri3-
 apt install -y  libxkbcommon0 libxkbcommon-dev libxxf86vm-dev
 apt install -y  build-essential libssl-dev libelf-dev bc texlive-font-utils
 apt install -y  libexpat1-dev libinput-dev xwayland mesa-utils-extra
-apt install -y  libc6-dev-x32-i386-cross libzstd-dev libx11-xcb-dev
+apt install -y  libzstd-dev libx11-xcb-dev
 apt install -y  libunwind-dev libxcb-dri2-0-dev python3-distutils libxshmfence-dev
 apt install -y  libxrandr-dev libxdamage-dev gettext valgrind libxcb-glx0-dev
 apt install -y  libxcb-shm0-dev libxkbfile-dev xterm libxcb-present-dev libfdt1
@@ -39,6 +39,7 @@ apt install -y  libxcb-dri2-0-dev:i386 libxcb-dri3-dev:i386 libxcb-present-dev:i
 apt install -y  libxxf86vm-dev:i386 libxrandr-dev:i386 libvulkan1:i386
 apt install -y  libvulkan-dev:i386 libselinux1:i386 libselinux1-dev:i386 libxcb-shm0:i386 libxcb-shm0-dev:i386
 apt install -y  libxml2-dev:i386 automake:i386 libc6-dev:i386 libffi-dev:i386
+apt install -y gcc-i686-linux-gnu g++-i686-linux-gnu libc6-dev-x32-i386-cross
 
 # Make sure we have libc packages correctly installed
 if [ "$(dpkg -s linux-libc-dev:amd64 | grep ^Version:)" !=  "$(dpkg -s linux-libc-dev:i386 | grep ^Version:)" ]; then
