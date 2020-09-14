63a622b3420413178dd3a96417bfbcadb944c602
Kalyan Kondapally <kalyan.kondapally@intel.com>
2020-09-14 04:30:34 -0700

    Remove obsolete paths.
    Signed-off-by: Kalyan Kondapally <kalyan.kondapally@intel.com>

--- chroot_environment.sh
+++ chroot_environment.sh
@@ -16,9 +16,6 @@ sudo mount build/output/rootfs.ext4 $MOUNT_POINT
 sudo mount -t proc /proc $MOUNT_POINT/proc
 sudo mount -o bind /run/shm $MOUNT_POINT/dev/shm
 sudo mount -o bind /dev/pts $MOUNT_POINT/dev/pts
-sudo mount -o bind $PWD/../cros_vm $MOUNT_POINT/build/cros_vm
-sudo mount -o bind $PWD/../drm-intel $MOUNT_POINT/build/drm-intel
-sudo mount -o bind patches $MOUNT_POINT/build/patches
 
 sudo cp $PWD/scripts/package-builder.sh $MOUNT_POINT/build/
 sudo chroot $MOUNT_POINT su -
@@ -27,7 +24,4 @@ echo "unmounting /proc /dev/shm /dev/pts"
 sudo umount -l $MOUNT_POINT/proc
 sudo umount -l $MOUNT_POINT/dev/shm
 sudo umount -l $MOUNT_POINT/dev/pts
-sudo umount -l $MOUNT_POINT/build/cros_vm
-sudo umount -l $MOUNT_POINT/build/drm-intel
-sudo umount -l $MOUNT_POINT/build/patches
 sudo umount -l $MOUNT_POINT
